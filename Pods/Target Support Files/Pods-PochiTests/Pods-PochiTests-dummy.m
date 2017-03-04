7cf29ab9a9b24014d2b223e7cbb1a2459aec6f55
Noritaka Kamiya <noritakakamiya@gmail.com>
2017-03-04 11:29:22 +0900

    setup

---
+++ Pods/Target Support Files/Pods-PochiTests/Pods-PochiTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PochiTests : NSObject
+@end
+@implementation PodsDummy_Pods_PochiTests
+@end
---
+++ Pods/Target Support Files/Pods-PochiUITests/Pods-PochiUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PochiUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PochiUITests
+@end
