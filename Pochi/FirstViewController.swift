8f286397df6533399a49229e7d5fb3a82336b378
Noritaka Kamiya <noritakakamiya@gmail.com>
2017-03-04 12:03:39 +0900

    Add get battery, run

--- Pochi/FirstViewController.swift
+++ Pochi/FirstViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import ExternalAccessory
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, Ev3ConnectionChangedDelegate {
     
     var connection: Ev3Connection?
     var brick: Ev3Brick?
@@ -19,6 +19,25 @@ class FirstViewController: UIViewController {
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(accessoryDisconnected), name: EAAccessoryDidDisconnectNotification, object: nil)
         EAAccessoryManager.sharedAccessoryManager().registerForLocalNotifications()
         print(EAAccessoryManager.sharedAccessoryManager().connectedAccessories.count)
+        
+        let accessory = getEv3Accessory()
+        connect(accessory!)
+    }
+    
+    private func getEv3Accessory() -> EAAccessory? {
+        let man = EAAccessoryManager.sharedAccessoryManager()
+        let connected = man.connectedAccessories
+        
+        for tmpAccessory in connected{
+            if Ev3Connection.supportsEv3Protocol(tmpAccessory){
+                return tmpAccessory
+            }
+        }
+        return nil
+    }
+    
+    func ev3ConnectionChanged(connected: Bool) {
+        print(connected)
     }
     
     func accessoryConnected(notification: NSNotification) {
@@ -36,6 +55,7 @@ class FirstViewController: UIViewController {
     
     private func connect(accessory: EAAccessory){
         connection = Ev3Connection(accessory: accessory)
+        connection?.connectionChangedDelegates.append(self)
         brick = Ev3Brick(connection: connection!)
         connection?.open()
     }
@@ -44,12 +64,27 @@ class FirstViewController: UIViewController {
         print(#function)
     }
 
+    @IBAction func batteryLevel(sender: AnyObject) {
+        self.brick?.directCommand.getBatteryLevel{ [weak self] (level) in
+            
+            let controller = UIAlertController(title: "Battery Level", message: "\(level)", preferredStyle: UIAlertControllerStyle.Alert)
+            controller.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+            self?.presentViewController(controller, animated: true, completion: {})
+        }
+    }
+    
+    @IBAction func run(sender: AnyObject) {
+        self.brick?.directCommand.turnMotorAtSpeed(onPorts: OutputPort.All, withSpeed: 50)
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+        self.brick?.directCommand.turnMotorAtSpeed(onPorts: OutputPort.All, withSpeed: 0)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
         setup()
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
